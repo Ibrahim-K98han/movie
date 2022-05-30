7f27a09e6463df2571baa969077ca6bfe7096bad
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-05-30 12:21:02 +0600

    movie name change

--- lib/tempdb/tempdb.dart
+++ lib/tempdb/tempdb.dart
@@ -2,7 +2,7 @@ import 'package:movie/models/models.dart';
 
 final movieList = [
   Movie(id: 1,name: "Ra One",subTitle: "SRK kids movie",type: "Action",description: "hello hello hello hello hello",rating: 4.2,image: 'images/movie1.jpg'),
-  Movie(id: 2,name: "Ra Two",subTitle: "SRK kids movie",type: "Comedy",description: "hello hello hello hello hello",rating: 5.0,image: 'images/movei2.jpg'),
-  Movie(id: 3,name: "Ra Three",subTitle: "SRK kids movie",type: "Kids",description: "hello hello hello hello hello",rating: 3.2,image: 'images/movie3.jpg'),
-  Movie(id: 4,name: "Ra Four",subTitle: "SRK kids movie",type: "Action",description: "hello hello hello hello hello",rating: 5.2,image: 'images/movie4.jpg')
+  Movie(id: 2,name: "Ra Two",subTitle: "Action kids movie",type: "Comedy",description: "hello hello hello hello hello",rating: 5.0,image: 'images/movei2.jpg'),
+  Movie(id: 3,name: "Ra Three",subTitle: "Action movie",type: "Kids",description: "hello hello hello hello hello",rating: 3.2,image: 'images/movie3.jpg'),
+  Movie(id: 4,name: "Ra Four",subTitle: "Funny movie",type: "Action",description: "hello hello hello hello hello",rating: 5.2,image: 'images/movie4.jpg')
 ];
